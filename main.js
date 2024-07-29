cf458d4e502d5f2511098ecd1e97dba3b5179785
Daeoss <daeossss@gmail.com>
2024-07-29 13:14:46 +0300

    Sound effects and menu

---
+++ main.js
@@ -0,0 +1,23 @@
+// Import the scene classes
+import StartMenu from './Scripts/StartMenu.js';
+import GameScene from './Scripts/GameScene.js';
+import EndGameScene from './Scripts/EndGameScene.js';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: [StartMenu, GameScene, EndGameScene],
+    pixelArt: true,
+    disableContextMenu: true, //Disable right mouse button menu
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+};
+
+const game = new Phaser.Game(config);
+
